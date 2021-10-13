f592da32ed6aa1420ebad6dab0fd0701739c585a
Pszione <oused.games@gmail.com>
2021-10-13 14:36:52 -0300

    Initial Commit

---
+++ android/app/src/main/kotlin/com/ousedgames/unitconverter/flutter_udacity_course/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.ousedgames.unitconverter.flutter_udacity_course
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,22 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const UnitConverterApp());
+}
+
+class UnitConverterApp extends StatelessWidget {
+  const UnitConverterApp({Key? key}) : super(key: key);
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'Unit Converter',
+      home: Scaffold(
+        backgroundColor: Colors.green[100],
+        body: const Center(),
+      ),
+    );
+  }
+}
