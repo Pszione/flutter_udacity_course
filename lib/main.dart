994598c068efeb0fe794960ad4f9329efa199d00
Pszione <oused.games@gmail.com>
2021-10-13 14:48:22 -0300

    UnitConverter - main.dart | ThemeData

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
 
 void main() {
   runApp(const UnitConverterApp());
@@ -17,6 +18,40 @@ class UnitConverterApp extends StatelessWidget {
         backgroundColor: Colors.green[100],
         body: const Center(),
       ),
+      theme: ThemeData(
+        brightness: Brightness.dark,
+        colorScheme: appDarkColorScheme,
+        primaryColor: kDarkBackgroundColor,
+        primaryColorDark: kWhiteColor,
+        scaffoldBackgroundColor: kDarkBackgroundColor,
+        backgroundColor: kDarkBackgroundColor,
+        dialogBackgroundColor: kSecondaryColor,
+        fontFamily: GoogleFonts.poppins().fontFamily,
+      ),
     );
   }
+
+  static ColorScheme appDarkColorScheme = const ColorScheme.dark(
+    primary: kPrimaryColor,
+    secondary: kSecondaryColor,
+    primaryVariant: kPrimaryColor,
+    secondaryVariant: kComplementaryColor,
+    surface: kDarkBackgroundColor,
+    background: kDarkBackgroundColor,
+  );
 }
+
+// temporarily placed here
+const kPrimaryColor = Color(0xFF1A73E9);
+const kSecondaryColor = Color(0xffff7b67);
+const kTertiaryColor = Color(0xfffcd76b);
+const kComplementaryColor = Color(0xff00444f);
+const kDarkBackgroundColor = Color(0xff343a44);
+const kWhiteColor = Colors.white;
+const kDarkColor = Color(0xFF191923);
+const kGrayColor = Color(0xFFD8D8D8);
+
+var kHugeBorderRadius = BorderRadius.circular(28);
+var kDefaultBorderRadius = BorderRadius.circular(kDefaultBorderRadiusAsDouble);
+const kDefaultBorderRadiusAsDouble = 20.0;
+const kSmallBorderRadiusAsDouble = 16.0;
